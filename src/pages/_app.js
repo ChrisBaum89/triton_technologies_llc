b9452db786c6fd637e4e0befd16ac38ab9c8f6c6
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-26 20:41:27 -0500

    added basic card layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,25 @@
 import '@/styles/globals.css'
-import CategoryCard from './components/category_card'
+import Image from 'next/image'
 
-export default function App({ Component, pageProps }) {
-  return(
+export default function App() {
+
+
+  return (
     <div>
-       <div>
-        
-       </div>
+      <div className="logo">
+        CRB Consulting
+      </div>
+      <div className="category-cards">
+        <div className="card">
+          Machine Control
+        </div>
+        <div className="card">
+          Machine Vision
+        </div>
+        <div className="card">
+          System Qualification and Validation
+        </div>
+      </div>
     </div>
   )
 }
