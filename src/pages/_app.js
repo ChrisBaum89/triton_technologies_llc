e5fa0e5829f523e9b057c2aa965291108c399ac6
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-26 20:55:20 -0500

    added note about useEffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@ import '@/styles/globals.css'
 import {useEffect} from 'react';
 
 export default function App() {
+  // Bootstrap uses some only client-side objects (window, document) to handle events. On the other hand, Next.js 
+  // renders the app on both the server side and the client side. To avoid the mentioned errors, we have to make 
+  //sure that the window and document objects only be used on the client side. That can be done with the useEffect hook
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap');
   }, [])
