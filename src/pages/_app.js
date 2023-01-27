cc64f366bc823218d7fc97838ca13f890672e5a7
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-26 20:30:41 -0500

    setting up basic divs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import CategoryCard from './components/category_card'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+       <div>
+        
+       </div>
+    </div>
+  )
 }
