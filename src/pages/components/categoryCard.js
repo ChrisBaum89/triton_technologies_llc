6201ae37dea4c0d48d8a805d960ee46c4888a01b
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-27 00:39:59 -0500

    changed class to className

--- src/pages/components/categoryCard.js
+++ src/pages/components/categoryCard.js
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export default function CategoryCard(props) {
     return(
-        <div class="card">
-            <div class="card-image">
-            <Image src={props.image} class="card-img-top" alt="..." style={{height: "12rem", width: "14rem"}}/>
+        <div className="card">
+            <div className="card-image">
+            <Image src={props.image} className="card-img-top" alt="..." style={{height: "12rem", width: "14rem"}}/>
             </div>
-            <div class="card-body">
-              <div class="card-title">{props.title}</div>
+            <div className="card-body">
+              <div className="card-title">{props.title}</div>
             </div>
         </div>
     )
--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -2,15 +2,15 @@
 export default function NavBar() {
     return (
         <div className="top-nav">
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">CRB Consulting</a>
-                    <ul class="navbar-nav me-right mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">About</a>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">CRB Consulting</a>
+                    <ul className="navbar-nav me-right mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
