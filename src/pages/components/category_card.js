63c943d336a305a276bbc57183ed17b3422f6948
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-26 23:50:19 -0500

    added Image div for styling

--- src/pages/components/category_card.js
+++ src/pages/components/category_card.js
@@ -2,8 +2,10 @@ import Image from "next/image"
 
 export default function CategoryCard(props) {
     return(
-        <div class="card" style={{width: "25rem"}}>
-          <Image src={props.image} class="card-img-top" alt="..." style={{height: "18rem", width: "25rem"}}/>
+        <div class="card">
+            <div class="card-image">
+            <Image src={props.image} class="card-img-top" alt="..." style={{height: "12rem", width: "14rem"}}/>
+            </div>
             <div class="card-body">
               <div class="card-title">{props.title}</div>
             </div>
