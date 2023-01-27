a3ec8289a845f7cd5a0bcbd85ea15db19cc556eb
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-27 13:42:47 -0500

    updated navbar style

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -2,15 +2,23 @@
 export default function NavBar() {
     return (
         <div className="top-nav">
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className="navbar navbar-expand">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">CRB Consulting</a>
-                    <ul className="navbar-nav me-right mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-right mb-0 mb-l-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">About</a>
+                            <a className="nav-link active" aria-current="page" href="#">
+                                <div className="link-text">
+                                    About
+                                </div>
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Contact</a>
+                            <a className="nav-link active" aria-current="page" href="#">
+                                <div className="link-text">
+                                    Contact
+                                </div>
+                            </a>
                         </li>
                     </ul>
                 </div>
