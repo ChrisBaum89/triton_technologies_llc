e3745556b3d9315e3b5c5c4670aef6e693439ea3
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-29 13:45:20 -0500

    added close button

--- src/pages/components/humanMachineInterfaces.js
+++ src/pages/components/humanMachineInterfaces.js
@@ -15,6 +15,9 @@ export default function HumanMachineInterfaces() {
                             <li>Proface</li>
                         </ul>
                     </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
