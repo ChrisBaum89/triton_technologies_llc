682558ed0031ef0d026e98ce8cfdca6301de9c31
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-29 10:55:55 -0500

    new file

---
+++ src/pages/components/about.js
@@ -0,0 +1,17 @@
+
+export default function About () {
+    return (
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample1" aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasLabel">About</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        CRB Consulting LLC was founded in 2022 by Chris Baum.
+                        Working in the pharmaceuticala and medical device industry for over a decade as an integrator
+                        and contract manufacturer, Chris has had a significant amount of experience supporting, upgrading, designing
+                        , and procuring automated manufacturing equipment with extensive experience in machine vision.
+                    </div>
+                </div>
+    )
+}
---
+++ src/pages/components/contact.js
@@ -0,0 +1,14 @@
+
+export default function Contact() {
+    return (
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample2" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasLabel">Contact</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                Need to add contact form
+            </div>
+        </div>
+    )
+}
