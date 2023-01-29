40257f2b4d4dc4ce145cd0f53df2ee889a073d8c
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-29 13:46:52 -0500

    added footer with close button

--- src/pages/components/systemTesting.js
+++ src/pages/components/systemTesting.js
@@ -16,6 +16,9 @@ export default function SystemTesting() {
                             <li>Software Validation (SV)</li>
                         </ul>
                     </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
             </div>
         </div>
