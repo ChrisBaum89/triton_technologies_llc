5ce027aecbbed19a948ad71b6fda1c1a1cd95b57
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-28 21:16:11 -0500

    added logo to navbar

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -1,10 +1,14 @@
 
-export default function NavBar() {
+import Image from "next/image"
+
+export default function NavBar(props) {
     return (
         <div className="top-nav">
             <nav className="navbar navbar-expand">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">CRB Consulting</a>
+                    <a className="navbar-brand" href="#">
+                    <Image src={props.logo} className="card-img-top" alt="..." style={{ height: "2rem", width: "auto" }} />
+                    </a>
                     <ul className="navbar-nav me-right mb-0 mb-l-0">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">
