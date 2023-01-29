962edcd30564d3d0509443def1e92f0880806485
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-29 13:14:16 -0500

    changed image href

--- src/pages/components/navBar.js
+++ src/pages/components/navBar.js
@@ -8,18 +8,18 @@ export default function NavBar(props) {
         <div className="top-nav">
             <nav className="navbar navbar-expand">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="">
                         <Image src={props.logo} className="card-img-top" alt="..." style={{ height: "2rem", width: "auto" }} />
                     </a>
                     <ul className="navbar-nav me-right mb-0 mb-l-0">
-                        <li className="nav-item">
+                        <li key={1} className="nav-item">
                             <a className="nav-link active" data-bs-toggle="offcanvas" href="#offcanvasExample1" role="button" aria-controls="offcanvasExample1">
                                 <div className="link-text">
                                     About
                                 </div>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li key={2} className="nav-item">
                             <a className="nav-link active" data-bs-toggle="offcanvas" href="#offcanvasExample2" role="button" aria-controls="offcanvasExample2">
                                 <div className="link-text">
                                     Contact
