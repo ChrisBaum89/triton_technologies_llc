7848475a273eb1e5a56c7be7dfd44e77ffa4090e
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-30 10:12:07 -0500

    changed to email/phone and title size

--- src/pages/components/contact.js
+++ src/pages/components/contact.js
@@ -2,32 +2,18 @@ export default function Contact() {
     return (
         <div className="offcanvas offcanvas-end offcanvas-size-xl" tabindex="-1" id="offcanvas2" aria-labelledby="offcanvas2Label">
             <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasLabel">Contact</h5>
+                <h1 className="offcanvas-title" id="offcanvasLabel">Contact</h1>
                 <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div className="offcanvas-body">
                 <form>
                     <div class="mb-3">
-                        <div className="contact-form-line">
-                            <label class="contact-label" for="first-name">First Name</label>
-                            <input className="contact-input" type="text" id="first-name" name="first-name"></input>
+                        <div>
+                            Email:  <a href = "mailto: crbconsultingsc@gmail.com">crbaumconsultingsc@gmail.com</a>
                         </div>
-                        <div className="contact-form-line">
-                            <label class="contact-label" for="last-name">Last Name</label>
-                            <input className="contact-input" type="text" id="last-name" name="last-name"></input>
+                        <div>
+                            Phone Number: 704-604-6450
                         </div>
-                        <div className="contact-form-line">
-                            <label class="contact-label" for="email">Email</label>
-                            <input className="contact-input" type="text" id="email" name="email"></input>
-                        </div>
-                        <div className="contact-form-line">
-                            <label class="contact-label" for="company">Company</label>
-                            <input className="contact-input" type="text" id="company" name="company"></input>
-                        </div>
-                        <div class="contact-message-line">
-                            <textarea class="contact-message" id="message" name="contact-message" rows="8" cols="30" placeholder="message"></textarea>
-                        </div>
-
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
