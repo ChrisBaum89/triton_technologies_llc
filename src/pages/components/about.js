15d272ae615bafee14fdbe1b387209cb2654649d
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-30 10:11:57 -0500

    changed title size

--- src/pages/components/about.js
+++ src/pages/components/about.js
@@ -1,9 +1,9 @@
 
 export default function About () {
     return (
-        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvas1" aria-labelledby="offcanvas1Label">
+        <div className="offcanvas offcanvas-end offcanvas-size-xl" tabindex="-1" id="offcanvas1" aria-labelledby="offcanvas1Label">
                     <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasLabel">About</h5>
+                        <h1 className="offcanvas-title" id="offcanvasLabel">About</h1>
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
