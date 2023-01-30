7d20fa8dcc4616959743a03eab062003267fb343
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-30 09:55:26 -0500

    changed offcanvas id and label

--- src/pages/components/contact.js
+++ src/pages/components/contact.js
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample2" aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvas2" aria-labelledby="offcanvas2Label">
             <div className="offcanvas-header">
                 <h5 className="offcanvas-title" id="offcanvasLabel">Contact</h5>
                 <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
