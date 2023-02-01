079712c8dd0a80fa35bbe25e355837d8941ba7fc
Chris Baum <Christopher.Baum@valpo.edu>
2023-02-01 06:36:22 -0500

    removed header exit button

--- src/pages/components/machineVision.js
+++ src/pages/components/machineVision.js
@@ -6,7 +6,6 @@ export default function MachineVision() {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="categoryModalLabel">Machine Vision</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
                     <div className="category-summary">
--- src/pages/components/systemTesting.js
+++ src/pages/components/systemTesting.js
@@ -6,7 +6,6 @@ export default function SystemTesting() {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="categoryModalLabel">System Qualification</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
                         <div className="category-summary">
