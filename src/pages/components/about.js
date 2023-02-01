a57191bb075f1dec043c93f2f36b755f5605aeae
Chris Baum <Christopher.Baum@valpo.edu>
2023-01-31 23:13:31 -0500

    updated content

--- src/pages/components/about.js
+++ src/pages/components/about.js
@@ -7,10 +7,20 @@ export default function About () {
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
-                        CRB Consulting LLC was founded in 2022 by Chris Baum.
-                        Working in the pharmaceuticala and medical device industry for over a decade as an integrator
-                        and contract manufacturer, Chris has had a significant amount of experience supporting, upgrading, designing
-                        , and procuring automated manufacturing equipment with extensive experience in machine vision.
+                    <p>Our goal as a company is to provide superior service, help our customers navigate challenges, and shed light on
+                        opportunities.</p>
+                        
+                        <p>Triton Technologies, LLC was founded in 2022 by Chris Baum and serves Upstate South Carolina.
+                        Working as both an integrator and for a high volume contract manufacturer in the Pharmaceutical and Medical
+                        Device industry, Chris was able to see the importance of quality machine controls and machine vision in a Production
+                        process. It can mean all of the difference in providing superior, safer, and more profitable products that make the
+                        manufacturer and end-customer happy.
+                        </p>
+                        <p>
+                        We know a lot goes into bringing a piece of automated equipment from start to finish and beyond, and we would like to help.
+                        Having designed, procured, installed, debugged, qualified, supported, and done continuous improvement on over 20+ pieces of
+                        custom automated equipment, we have the expertise to assist you. 
+                        </p>
                     </div>
                 </div>
     )
