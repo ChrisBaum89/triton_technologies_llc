a89b80e180e34966907e62b6c1989631cb0c2aff
Chris Baum <Christopher.Baum@valpo.edu>
2023-02-01 16:48:18 -0500

    updated content

--- src/pages/components/about.js
+++ src/pages/components/about.js
@@ -1,26 +1,21 @@
 
 export default function About () {
     return (
-        <div className="offcanvas offcanvas-end offcanvas-size-xl" tabindex="-1" id="offcanvas1" aria-labelledby="offcanvas1Label">
+        <div className="offcanvas offcanvas-end offcanvas-size-xl" tabIndex="-1" id="offcanvas1" aria-labelledby="offcanvas1Label">
                     <div className="offcanvas-header">
                         <h1 className="offcanvas-title" id="offcanvasLabel">ABOUT</h1>
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
-                    <p>Our goal as a company is to provide superior service, help our customers navigate challenges, and shed light on
-                        opportunities.</p>
-                        
-                        <p>Triton Technologies, LLC was founded in 2022 by Chris Baum and serves Upstate South Carolina.
-                        Working as both an integrator and for a high volume contract manufacturer in the Pharmaceutical and Medical
-                        Device industry, Chris was able to see the importance of quality machine controls and machine vision in a Production
-                        process. It can mean all of the difference in providing superior, safer, and more profitable products that make the
-                        manufacturer and end-customer happy.
+                        <p>Founded in 2022, Triton Technologies, LLC is a specialized automation integration business that focuses 
+                            on machine control and machine vision solutions. The company helps clients optimize their operations 
+                            by providing cutting-edge technology and expert services to automate processes and improve efficiency. 
+                            With a deep understanding of machine control and machine vision, Triton Technologies is committed to 
+                            delivering high-quality results to its clients and driving innovation in the field of automation integration.
                         </p>
-                        <p>
-                        We know a lot goes into bringing a piece of automated equipment from start to finish and beyond, and we would like to help.
-                        Having designed, procured, installed, debugged, qualified, supported, and done continuous improvement on over 20+ pieces of
-                        custom automated equipment, we have the expertise to assist you. 
-                        </p>
+                        <p>Our goal as a company is to provide superior service, help our customers navigate challenges, and shed light on
+                        opportunities to improve processes.</p>
+
                     </div>
                 </div>
     )
